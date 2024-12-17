a8a2472fec870d51b5df6c1ef6ec00b53b8886a3
Mahendra <mahendrapurohit9889@gmail.com>
2024-12-17 16:42:17 +0530

    code

--- Pattern.c
+++ Pattern.c
@@ -14,3 +14,28 @@ int main() {
         }
     }
 }
+
+#include <stdio.h>
+
+int main() {
+    
+    int n=5;
+    int i,j;
+    
+    for(i=1;i<=n;i++)
+    {
+        for(j=n;j>=1;j--)
+        {
+            if(i>=j)
+            {
+                printf("* ");
+            } else {
+                
+                printf(" ");
+            }
+                
+        }
+            printf("\n");
+    
+    }
+}
