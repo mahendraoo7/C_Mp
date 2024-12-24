1ec10c208ad8c7a47c31fe7d8b76c10d2c77340b
Mahendra <mahendrapurohit9889@gmail.com>
2024-12-24 11:58:58 +0530

    code

--- Pattern.c
+++ Pattern.c
@@ -15,27 +15,93 @@
 //     }
 // }
 
+// #include <stdio.h>
+
+// int main() {
+    
+//     int n=5;
+//     int i,j;
+    
+//     for(i=1;i<=n;i++)
+//     {
+//         for(j=n;j>=1;j--)
+//         {
+//             if(i>=j)
+//             {
+//                 printf("* ");
+//             } else {
+                
+//                 printf(" ");
+//             }
+                
+//         }
+//             printf("\n");
+    
+//     }
+// }
+
+
+#include <stdio.h>
+
+// int main () {
+    
+//     int i,j;
+
+//     for( i = 1; i <= 5; i++)
+//     {
+//         printf("\n");
+//         for(j = 5; j >= i; j--)
+//         {
+//             printf("* ");
+            
+//         }
+//     }
+
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     int i,j,k;
+
+//     for(i=0;i<=5;i++)
+//     {
+//         for(j=0; j<= 5-i;j++)
+//         {
+//             printf(" ");
+//         }
+
+//         for(k=0;k<=5;k++)
+//         {
+//             printf("*");
+//         }
+
+//         printf("\n");
+//     }
+// }
+
 #include <stdio.h>
 
 int main() {
-    
-    int n=5;
-    int i,j;
-    
-    for(i=1;i<=n;i++)
+
+    int i,j,k;
+
+    int A = 'A';
+
+    for(i=1;i<=5;i++)
     {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=2*i;j++)
         {
-            if(i>=j)
-            {
-                printf("* ");
-            } else {
-                
-                printf(" ");
-            }
+            printf(" ");
+
+        }
+       for(k=1;k<=2*(5-i)-1;k++)
+        {
+                printf("%c ",A++);
                 
         }
-            printf("\n");
-    
+
+        printf("\n");
+        
     }
 }
